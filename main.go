1ffac35519b945f7934e021de1e863f1c23c9c60
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-09-10 17:06:07 +1000

    Better panic

--- main.go
+++ main.go
@@ -9,8 +9,7 @@ import (
 func main() {
 	botToken, ok := os.LookupEnv("BOT_TOKEN")
 	if !ok {
-		log.Printf("Value not found : %v \n", ok)
-		log.Panic("Your bot token is probably not set, please make sure BOT_TOKEN is present in your .env file")
+		log.Panic("Bot token was not found, please make sure BOT_TOKEN is present in your .env file")
 	}
 
 	fmt.Printf("Value found : %s \n", botToken)
