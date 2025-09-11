e4334203746c660636235d08ced8ccd853f16ca2
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-09-12 01:30:22 +1000

    New icons + helper.go

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"log"
-	"os"
-	"strconv"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -17,34 +15,6 @@ func init() {
 	if err != nil {
 		log.Panic("Error creating bot instance, please make sure BOT_TOKEN is a valid Discord bot token")
 	}
-
-}
-
-func lookup(envString string, defaultValue string) string {
-	value, ok := os.LookupEnv(envString)
-	if !ok {
-		log.Printf("Optional env variable %s was not found, using default value %s \n", envString, defaultValue)
-		return defaultValue
-	}
-	return value
-}
-
-func lookupRequired(envString string) string {
-	value, ok := os.LookupEnv(envString)
-	if !ok {
-		log.Panicf("Required env variable %s was not found, please make sure it is present in your .env file \n", envString)
-	}
-	return value
-}
-
-func lookupBool(envString string, defaultValue bool) bool {
-	l := lookup("REMOVE_COMMANDS_ON_EXIT", "false")
-	v, err := strconv.ParseBool(l)
-	if err != nil {
-		log.Printf("Error parsing env variable %s: expected bool, received %s \n", envString, l)
-		return defaultValue
-	}
-	return v
 }
 
 func main() {
