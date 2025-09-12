0213097d414147316c5c9c30077325c348587d24
Bittorn <48916178+Bittorn@users.noreply.github.com>
2025-09-12 22:55:13 +1000

    Implementing slash commands

--- main.go
+++ main.go
@@ -13,11 +13,30 @@ import (
 var session *discordgo.Session
 
 var (
-	botToken             = lookupRequired("BOT_TOKEN")
-	guildID              = lookup("GUILD_ID", "")
-	removeCommandsOnExit = lookupBool("REMOVE_COMMANDS_ON_EXIT", false)
+	botToken             = lookupRequired("BOT_TOKEN")                  //  Bot access token
+	guildID              = lookup("GUILD_ID", "")                       //  If not passed - bot registers commands globally
+	removeCommandsOnExit = lookupBool("REMOVE_COMMANDS_ON_EXIT", false) // Remove all commands after shutdowning or not, defaults to false
 
-	// commands = []*discordgo.ApplicationCommand{}
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name: "ping",
+			// All commands and options must have a description
+			// Commands/options without description will fail the registration
+			// of the command.
+			Description: "Pings the bot",
+		},
+	}
+
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+		"ping": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "Pong!",
+				},
+			})
+		},
+	}
 )
 
 const (
@@ -26,8 +45,6 @@ const (
 		discordgo.IntentMessageContent |
 		discordgo.IntentGuildScheduledEvents |
 		discordgo.IntentGuildMessagePolls)
-
-	prefix string = "!metro"
 )
 
 func init() {
@@ -37,33 +54,53 @@ func init() {
 		log.Panic("Error creating bot instance, please make sure BOT_TOKEN is a valid Discord bot token")
 	}
 
+	// Register command handlers
+	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+
 	session.Identify.Intents = botIntents
 }
 
 func main() {
-	session.AddHandler(sessionHandler)
+	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
 
 	err := session.Open()
 	if err != nil {
 		log.Fatalf("Cannot open the session: %v", err)
 	}
 
-	// log.Println("Adding commands...")
-	// registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
-	// for i, v := range commands {
-	// 	cmd, err := session.ApplicationCommandCreate(session.State.User.ID, guildID, v)
-	// 	if err != nil {
-	// 		log.Panicf("Cannot create '%v' command: %v", v.Name, err)
-	// 	}
-	// 	registeredCommands[i] = cmd
-	// }
+	log.Println("Registering commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, guildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
 
 	defer session.Close()
 
 	log.Println("Bot is now online!")
 
 	// Ensure the bot closes when interrupt signal is sent
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-stop
+
+	if removeCommandsOnExit {
+		for _, v := range registeredCommands {
+			err := session.ApplicationCommandDelete(session.State.User.ID, guildID, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
+	log.Println("Gracefully shutting down.")
 }
